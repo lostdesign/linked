1720fa2c46815a4a582faf2033afdba9af752e3d
lostdesign <info@lost.design>
2021-04-08 22:47:08 +0200

    ♻️ add icon loader

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,36 @@
 module.exports = {
-    configureWebpack: {
-        devtool: 'source-map'
-    },
-    pluginOptions: {
-        electronBuilder: {
-            nodeIntegration: true,
-            builderOptions: {
-                mac: {
-                    hardenedRuntime: true,
-                    entitlements: "./build/entitlements.mac.inherit.plist"
-                },
-                linux: {
-                  target: ["AppImage"]
-                },
-                publish: ['github'],
-                appId: 'design.lost.linked',
-                afterSign: './afterSignHook.js'
-            }
-        }
+
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-loader')
+      .loader('vue-loader') // or `vue-loader-v16` if you are using a preview support of Vue 3 in Vue CLI
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+
+  configureWebpack: {
+      devtool: 'source-map'
+  },
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        mac: {
+          hardenedRuntime: true,
+          entitlements: "./build/entitlements.mac.inherit.plist"
+        },
+        linux: {
+          target: ["AppImage"]
+        },
+        publish: ['github'],
+        appId: 'design.lost.linked',
+        afterSign: './afterSignHook.js'
+      }
     }
+  }
 }
