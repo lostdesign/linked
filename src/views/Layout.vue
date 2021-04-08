c3dd2a59eaceade076b37a9955073066b5ad5001
lostdesign <info@lost.design>
2021-04-08 22:46:56 +0200

    💄 fix layout

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col bg-white text-black dark:bg-black">
+  <div class="flex flex-col bg-white text-black dark:bg-black min-h-screen">
     <div class="text-white w-full h-full">
       <slot />
     </div>
