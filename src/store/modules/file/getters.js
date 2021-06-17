7296f9481e5d3f3750f0de7c136bf56181fb3290
lostdesign <info@lost.design>
2021-06-17 10:58:44 +0200

    ✨ add file store module

---
+++ src/store/modules/file/getters.js
@@ -0,0 +1,16 @@
+import { Getters } from './types'
+
+export default {
+  [Getters.GET_CONTENT](state) {
+    return state.content
+  },
+  [Getters.GET_RATING](state) {
+    return state.rating
+  },
+  [Getters.GET_FILE](state) {
+    return {
+      content: state.content,
+      rating: state.rating
+    }
+  }
+}
