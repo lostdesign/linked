3634fbef70bf996ee01e84230d50a405206d9630
lostdesign <info@lost.design>
2021-06-17 10:59:49 +0200

    ♻️ move shortcuts, refactor mixin to store

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
+import {
+  Getters as CalendarGetters,
+  Actions as CalendarActions
+} from '@/store/modules/calendar/types'
+
+import { Actions as FileActions } from '@/store/modules/file/types'
+
 const { ipcRenderer } = require('electron')
 
 export default {
@@ -21,12 +30,50 @@ export default {
       }
     }
   },
+  methods: {
+    ...mapActions('calendar', [
+      CalendarActions.SET_DATE,
+      CalendarActions.SET_DAY_TO
+    ]),
+    ...mapActions('file', [FileActions.FETCH_FILE])
+  },
+  computed: {
+    ...mapGetters('calendar', [CalendarGetters.GET_CURRENT_DATE])
+  },
   mounted() {
     ipcRenderer.on('open-settings', () => {
       this.$router.push('settings', () => {})
     })
+
+    ipcRenderer.on('set-today', () => {
+      this.setDate()
+    })
+
+    ipcRenderer.on('previous-day', () => {
+      this.setDayTo(-1)
+    })
+
+    ipcRenderer.on('next-day', () => {
+      this.setDayTo(1)
+    })
+
+    ipcRenderer.on('previous-week', () => {
+      this.setDayTo(-7)
+    })
+
+    ipcRenderer.on('next-week', () => {
+      this.setDayTo(7)
+    })
   },
   created() {
+    this.fetchFile(this.getCurrentDate)
+
+    this.$store.subscribe((mutation) => {
+      if (mutation.type === `calendar/${CalendarActions.SET_DATE}`) {
+        this.fetchFile(this.getCurrentDate)
+      }
+    })
+
     if (this.themeMode === 'dark' || !('theme' in localStorage)) {
       document.documentElement.classList.add('dark')
     } else {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,272 +1,64 @@
 <template>
   <Layout>
-    <main
-      class="flex-col items-stretch h-full min-h-screen"
-      @click="focusEditor"
-    >
-      <!-- Header Start -->
-      <header
-        class="
-          sticky
-          top-0
-          text-black
-          bg-white
-          dark:bg-black
-          dark:text-white
-          z-50
-          select-none
-        "
-      >
-        <week-switcher />
-
-        <!--
-          Day switcher,
-          already exists as own component, but currently isnt working, the editor content doesnt swap.
-
-          going to fix when move to vuex
-        -->
-        <div
-          class="
-            flex
-            dark:bg-black
-            justify-center
-            space-x-4
-            z-50
-            border-b border-gray-400
-            dark:border-gray-800
-            py-4
-          "
-        >
-          <template v-for="date in getCurrentWeekDates()">
-            <div
-              :key="date.day"
-              class="
-                flex-col
-                justify-center
-                items-center
-                self-center
-                text-center
-              "
-            >
-              <span
-                class="block mb-1 text-xs text-gray-400 dark:text-gray-700"
-                :class="{
-                  'text-red-400 dark:text-red-500': date.isoDate === today
-                }"
-              >
-                {{ date.weekDay }}
-              </span>
-              <span
-                class="
-                  flex
-                  justify-center
-                  items-center
-                  self-center
-                  text-center
-                  w-10
-                  h-10
-                  rounded-full
-                  font-black
-                  text-xs
-                  hover:bg-gray-200
-                  dark:hover:bg-gray-800
-                  cursor-pointer
-                  ring-red-600
-                  dark:ring-red-900
-                "
-                :class="{ 'ring-4 text-sm': date.isoDate === today }"
-                :key="date.day"
-                @click="setDay(date.isoDate)"
-              >
-                {{ date.day }}
-              </span>
-            </div>
-          </template>
-        </div>
-      </header>
-      <!-- Header End -->
-      <div v-if="editor">
-        <div class="px-10 mt-5 text-gray-400 dark:text-gray-500 relative">
-          <bubble-menu class="bubble-menu" :editor="editor" v-if="editor">
-            <button @click="editor.chain().focus().toggleHighlight().run()">
-              <PenIcon />
-            </button>
-            <button @click="editor.chain().focus().toggleBold().run()">
-              <BoldIcon />
-            </button>
-            <button @click="editor.chain().focus().toggleItalic().run()">
-              <ItalicIcon />
-            </button>
-            <button @click="editor.chain().focus().toggleStrike().run()">
-              <StrikeThroughIcon />
-            </button>
-          </bubble-menu>
-          <floating-menu class="floating-menu" :editor="editor" v-if="editor">
-            <button @click="editor.chain().focus().toggleTaskList().run()">
-              <CheckboxIcon />
-            </button>
-            <button @click="editor.chain().focus().toggleBulletList().run()">
-              <BulletListIcon />
-            </button>
-            <button @click="editor.chain().focus().toggleCodeBlock().run()">
-              <CodeIcon />
-            </button>
-          </floating-menu>
-          <div class="text-black dark:text-white">
-            <editor-content :editor="editor" v-model="content" />
-          </div>
-        </div>
-      </div>
+    <main class="flex-col items-stretch h-full min-h-screen">
+      <app-header />
+      <editor />
     </main>
   </Layout>
 </template>
 
 <script>
 import Layout from './Layout'
-import Calendar from '@/mixins/calendar'
-import File from '@/mixins/file'
-import WeekSwitcher from '@/components/week-switcher'
-import BulletListIcon from '@/assets/icons/bullet-list.svg'
-import CheckboxIcon from '@/assets/icons/checkbox.svg'
-import CodeIcon from '@/assets/icons/code.svg'
-import PenIcon from '@/assets/icons/pen.svg'
-import BoldIcon from '@/assets/icons/bold.svg'
-import ItalicIcon from '@/assets/icons/italic.svg'
-import StrikeThroughIcon from '@/assets/icons/strikethrough.svg'
+import AppHeader from '@/components/app-header'
+import Editor from '@/components/editor'
 
-import { Editor, EditorContent, BubbleMenu, FloatingMenu } from '@tiptap/vue-2'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import TaskList from '@tiptap/extension-task-list'
-import TaskItem from '@tiptap/extension-task-item'
-import Heading from '@tiptap/extension-heading'
-import Highlight from '@tiptap/extension-highlight'
-import CodeBlock from '@tiptap/extension-code-block'
-import BulletList from '@tiptap/extension-bullet-list'
-import ListItem from '@tiptap/extension-list-item'
-import Bold from '@tiptap/extension-bold'
-import Italic from '@tiptap/extension-italic'
-import Image from '@tiptap/extension-image'
-import HorizontalRule from '@tiptap/extension-horizontal-rule'
-import Strike from '@tiptap/extension-strike'
-import Link from '@tiptap/extension-link'
-import History from '@tiptap/extension-history'
-
-const { ipcRenderer } = require('electron')
+import { mapGetters, mapActions } from 'vuex'
+import {
+  Getters as CalendarGetters,
+  Actions as CalendarActions
+} from '@/store/modules/calendar/types'
 
 export default {
   components: {
     Layout,
-    EditorContent,
-    FloatingMenu,
-    BubbleMenu,
-    BulletListIcon,
-    CheckboxIcon,
-    CodeIcon,
-    PenIcon,
-    BoldIcon,
-    ItalicIcon,
-    StrikeThroughIcon,
-    WeekSwitcher
+    AppHeader,
+    Editor
   },
-  mixins: [Calendar, File],
   data() {
     return {
-      keysPressed: {},
-      editor: null
+      keysPressed: {}
     }
   },
   methods: {
-    focusEditor() {
-      this.editor.chain().focus().run()
-    }
+    ...mapActions('calendar', [
+      CalendarActions.SET_DATE,
+      CalendarActions.SET_DAY_TO
+    ])
+  },
+  computed: {
+    ...mapGetters('calendar', [CalendarGetters.GET_CURRENT_DATE])
   },
   mounted() {
-    this.editor = new Editor({
-      extensions: [
-        Document,
-        Paragraph,
-        Text,
-        TaskList,
-        TaskItem,
-        Heading,
-        Highlight,
-        CodeBlock,
-        BulletList,
-        ListItem,
-        Bold,
-        Italic,
-        Image,
-        HorizontalRule,
-        Strike,
-        Link,
-        History
-      ],
-      content: this.content,
-      autofocus: true,
-      onUpdate: ({ editor }) => {
-        this.content = editor.getHTML()
-        this.debounce(this.saveFile(), 500)
-      }
-    })
-
-    ipcRenderer.on('set-today', () => {
-      this.today = this.getToday()
-    })
-
-    ipcRenderer.on('previous-day', () => {
-      this.shiftDay(-1)
-    })
-
-    ipcRenderer.on('next-day', () => {
-      this.shiftDay(1)
-    })
-
-    ipcRenderer.on('previous-week', () => {
-      this.shiftDay(-7)
-    })
-
-    ipcRenderer.on('next-week', () => {
-      this.shiftDay(7)
-    })
-
     document.addEventListener('keydown', (event) => {
       this.keysPressed[event.key] = true
       const modifier = this.keysPressed['Shift'] && this.keysPressed['Control']
 
       if (modifier && event.code === 'Enter') {
-        this.today = this.getToday()
+        this.setDate()
       }
 
       if (modifier && event.code === 'ArrowLeft') {
-        this.shiftDay(-1)
+        this.setDayTo(-1)
       }
 
       if (modifier && event.code === 'ArrowRight') {
-        this.shiftDay(1)
+        this.setDayTo(1)
       }
     })
 
     document.addEventListener('keyup', (event) => {
       delete this.keysPressed[event.key]
     })
-  },
-
-  watch: {
-    content(value) {
-      const isSame = this.editor.getHTML() === value
-
-      if (isSame) {
-        return
-      }
-
-      this.editor.commands.setContent(this.content, false)
-    }
-  },
-
-  beforeDestroy() {
-    this.editor.destroy()
   }
 }
 </script>
