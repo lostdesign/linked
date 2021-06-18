bd4efd2161c231ca37f73ecfce758d3716579ed0
lostdesign <info@lost.design>
2021-06-18 19:23:26 +0200

    ♻️ refactor editor, extract extensions

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -49,23 +49,7 @@ import ItalicIcon from '@/assets/icons/italic.svg'
 import StrikeThroughIcon from '@/assets/icons/strikethrough.svg'
 
 import { Editor, EditorContent, BubbleMenu, FloatingMenu } from '@tiptap/vue-2'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import TaskList from '@tiptap/extension-task-list'
-import TaskItem from '@tiptap/extension-task-item'
-import Heading from '@tiptap/extension-heading'
-import Highlight from '@tiptap/extension-highlight'
-import CodeBlock from '@tiptap/extension-code-block'
-import BulletList from '@tiptap/extension-bullet-list'
-import ListItem from '@tiptap/extension-list-item'
-import Bold from '@tiptap/extension-bold'
-import Italic from '@tiptap/extension-italic'
-import Image from '@tiptap/extension-image'
-import HorizontalRule from '@tiptap/extension-horizontal-rule'
-import Strike from '@tiptap/extension-strike'
-import Link from '@tiptap/extension-link'
-import History from '@tiptap/extension-history'
+import { extensions } from './tiptap-extensions'
 
 export default {
   components: {
@@ -94,25 +78,7 @@ export default {
   },
   mounted() {
     this.editor = new Editor({
-      extensions: [
-        Document,
-        Paragraph,
-        Text,
-        TaskList,
-        TaskItem,
-        Heading,
-        Highlight,
-        CodeBlock,
-        BulletList,
-        ListItem,
-        Bold,
-        Italic,
-        Image,
-        HorizontalRule,
-        Strike,
-        Link,
-        History
-      ],
+      extensions,
       content: this.getContent,
       autofocus: true,
       onUpdate: ({ editor }) => {
---
+++ src/components/tiptap-extensions.js
@@ -0,0 +1,37 @@
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import TaskList from '@tiptap/extension-task-list'
+import TaskItem from '@tiptap/extension-task-item'
+import Heading from '@tiptap/extension-heading'
+import Highlight from '@tiptap/extension-highlight'
+import CodeBlock from '@tiptap/extension-code-block'
+import BulletList from '@tiptap/extension-bullet-list'
+import ListItem from '@tiptap/extension-list-item'
+import Bold from '@tiptap/extension-bold'
+import Italic from '@tiptap/extension-italic'
+import Image from '@tiptap/extension-image'
+import HorizontalRule from '@tiptap/extension-horizontal-rule'
+import Strike from '@tiptap/extension-strike'
+import Link from '@tiptap/extension-link'
+import History from '@tiptap/extension-history'
+
+export const extensions = [
+  Document,
+  Paragraph,
+  Text,
+  TaskList,
+  TaskItem,
+  Heading,
+  Highlight,
+  CodeBlock,
+  BulletList,
+  ListItem,
+  Bold,
+  Italic,
+  Image,
+  HorizontalRule,
+  Strike,
+  Link,
+  History
+]
