17a652293e5fcdd068e7e065e5729c1453fc2fbd
lostdesign <info@lost.design>
2021-06-18 19:30:32 +0200

    ♻️ refactor electron menu into module, clean code

--- src/services/menu-template.js
+++ src/services/menu-template.js
@@ -1,4 +1,137 @@
 import { app, BrowserWindow, shell } from 'electron'
 const isMacOS = process.platform === 'darwin'
 
-export 
+export const template = [
+  {
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      {
+        label: 'Settings',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('open-settings')
+            browserWindow.show()
+          })
+        },
+        accelerator: 'CMD + ,'
+      },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMacOS
+        ? [
+            { role: 'delete' },
+            { role: 'selectAll' },
+            { type: 'separator' },
+            {
+              label: 'Speech',
+              submenu: [{ role: 'startSpeaking' }, { role: 'stopSpeaking' }]
+            }
+          ]
+        : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }])
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Home',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('open-home')
+            browserWindow.show()
+          })
+        }
+      },
+      {
+        label: 'Search',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('open-search')
+            browserWindow.show()
+          })
+        },
+        accelerator: 'CMD + K'
+      }
+    ]
+  },
+  {
+    label: 'Calendar',
+    submenu: [
+      {
+        label: 'Today',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('set-today')
+            browserWindow.show()
+          })
+        },
+        accelerator: 'CTRL + SHIFT + ENTER'
+      },
+      { type: 'separator' },
+      {
+        label: 'Previous Day',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('previous-day')
+            browserWindow.show()
+          })
+        },
+        accelerator: 'CTRL + SHIFT + LEFT'
+      },
+      {
+        label: 'Next Day',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('next-day')
+            browserWindow.show()
+          })
+        },
+        accelerator: 'CTRL + SHIFT + RIGHT'
+      },
+      { type: 'separator' },
+      {
+        label: 'Previous Week',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('previous-week')
+            browserWindow.show()
+          })
+        }
+      },
+      {
+        label: 'Next Week',
+        click() {
+          BrowserWindow.getAllWindows().forEach(browserWindow => {
+            browserWindow.webContents.send('next-week')
+            browserWindow.show()
+          })
+        }
+      },
+      { type: 'separator' },
+      { role: 'reload' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Documentation',
+        click: () => {
+          shell.openExternal('https://uselinked.com/docs')
+        }
+      }
+    ]
+  }
+]
