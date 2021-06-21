d9b43dc7460ae099969773b66accbe9d11f747d2
lostdesign <info@lost.design>
2021-06-21 19:41:48 +0200

    ♻️ format locale index file

--- src/locales/index.js
+++ src/locales/index.js
@@ -4,19 +4,19 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 function loadLocaleMessages() {
   const locales = require.context(
-    "@/locales",
+    '@/locales',
     true,
     /[A-Za-z0-9-_,\s]+\.json$/i
-  );
-  const messages = {};
+  )
+  const messages = {}
   locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key);
+      const locale = matched[1]
+      messages[locale] = locales(key)
     }
-  });
-  return messages;
+  })
+  return messages
 }
 
 const i18n = new VueI18n({
@@ -25,5 +25,5 @@ const i18n = new VueI18n({
   messages: loadLocaleMessages()
 })
 
-export {loadLocaleMessages}
+export { loadLocaleMessages }
 export default i18n
