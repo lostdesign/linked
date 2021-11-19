0aabb4e24efa194fad352e3b8768adfea66fe4b3
Aitor <agomila96@gmail.com>
2021-11-19 16:57:39 +0100

    ✏️ Fix typo

--- src/updater.js
+++ src/updater.js
@@ -20,7 +20,7 @@ autoUpdater.on('update-available', async () => {
         autoUpdater.downloadUpdate()
     }
 })
-autoUpdater.on('update-not-available', () => {
+autoUpdater.on('update-not-available', async () => {
     await dialog.showMessageBox({
         title: 'Updates not available',
         message: "There isn't any update available. Sorry :(",
