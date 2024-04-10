a74b719e369a5c0656526bdfb9ce7ff2784a9d13
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-04-10 17:28:19 +0300

    refactor: remove unused code

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,38 +1,36 @@
 <script setup lang="ts">
-import {onBeforeUnmount, onMounted, ref} from "vue";
-import {watchDebounced} from "@vueuse/core";
-import {useCalendarStore} from "@/stores/useCalendarStore.ts";
-import {fetchContent, storeContent} from "@/utils/storage.ts";
-import {EditorContent, Editor} from "@tiptap/vue-3";
-import {Gapcursor} from "@tiptap/extension-gapcursor";
-import {CharacterCount} from "@tiptap/extension-character-count";
-import {Paragraph} from "@tiptap/extension-paragraph";
-import {TaskList} from "@tiptap/extension-task-list";
-import {TaskItem} from "@tiptap/extension-task-item";
-import {Heading} from "@tiptap/extension-heading";
-import {CodeBlock} from "@tiptap/extension-code-block";
-import {BulletList} from "@tiptap/extension-bullet-list";
-import {ListItem} from "@tiptap/extension-list-item";
-import {Bold} from "@tiptap/extension-bold";
-import {Italic} from "@tiptap/extension-italic";
-import {Link} from "@tiptap/extension-link";
-import {Highlight} from "@tiptap/extension-highlight";
-import {HorizontalRule} from "@tiptap/extension-horizontal-rule";
-import {Strike} from "@tiptap/extension-strike";
-import {Document} from "@tiptap/extension-document";
-import {Text} from "@tiptap/extension-text";
-import {History} from "@tiptap/extension-history";
-import Commands from "@/components/slash-commands/commands.ts";
-import suggestion from "@/components/slash-commands/suggestion.ts";
+import { onBeforeUnmount, onMounted, ref } from "vue";
+import { watchDebounced } from "@vueuse/core";
+import { useCalendarStore } from "@/stores/useCalendarStore.ts";
+import { fetchContent, storeContent } from "@/utils/storage.ts";
+import { EditorContent, Editor } from "@tiptap/vue-3";
+import { Gapcursor } from "@tiptap/extension-gapcursor";
+import { CharacterCount } from "@tiptap/extension-character-count";
+import { Paragraph } from "@tiptap/extension-paragraph";
+import { TaskList } from "@tiptap/extension-task-list";
+import { TaskItem } from "@tiptap/extension-task-item";
+import { Heading } from "@tiptap/extension-heading";
+import { CodeBlock } from "@tiptap/extension-code-block";
+import { BulletList } from "@tiptap/extension-bullet-list";
+import { ListItem } from "@tiptap/extension-list-item";
+import { Bold } from "@tiptap/extension-bold";
+import { Italic } from "@tiptap/extension-italic";
+import { Link } from "@tiptap/extension-link";
+import { Highlight } from "@tiptap/extension-highlight";
+import { HorizontalRule } from "@tiptap/extension-horizontal-rule";
+import { Strike } from "@tiptap/extension-strike";
+import { Document } from "@tiptap/extension-document";
+import { Text } from "@tiptap/extension-text";
+import { History } from "@tiptap/extension-history";
 
-const store = useCalendarStore()
-const editor = ref()
-const content = ref('')
+const store = useCalendarStore();
+const editor = ref();
+const content = ref("");
 
 onMounted(async () => {
   editor.value = new Editor({
     autofocus: true,
-    content: await fetchContent(store.currentDate) as string,
+    content: (await fetchContent(store.currentDate)) as string,
     editable: true,
     extensions: [
       Document,
@@ -64,32 +62,32 @@ onMounted(async () => {
         },
       }),*/
       Gapcursor,
-      CharacterCount
+      CharacterCount,
     ],
-    onUpdate: async ({editor}) => {
-      content.value = editor.getHTML()
-    }
-  })
-})
+    onUpdate: async ({ editor }) => {
+      content.value = editor.getHTML();
+    },
+  });
+});
 
-const focusEditor = () => editor.value.chain().focus().run()
+const focusEditor = () => editor.value.chain().focus().run();
 
 watchDebounced(
   content,
   async () => storeContent(store.currentDate, content.value),
-  {debounce: 500, maxWait: 1500},
-)
+  { debounce: 500, maxWait: 1500 }
+);
 
-onBeforeUnmount(() => console.log('destroying'))
+onBeforeUnmount(() => console.log("destroying"));
 </script>
 
 <template>
   <div
-    class='px-10 mt-5 text-gray-400 dark:text-gray-500 relative flex-grow cursor-text'
-    @click='focusEditor'
+    class="px-10 mt-5 text-gray-400 dark:text-gray-500 relative flex-grow cursor-text"
+    @click="focusEditor"
   >
-    <div class='text-black dark:text-white'>
-      <editor-content class='pb-10' :editor="editor" v-model="content"/>
+    <div class="text-black dark:text-white">
+      <editor-content class="pb-10" :editor="editor" v-model="content" />
     </div>
   </div>
 </template>
