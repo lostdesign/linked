737257be7fd57e47d7936b234e0a266cfe161743
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-04-16 22:57:26 +0300

    chore: remove prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/recommended"],
-  eslintConfigPrettier,
   {
     ignores: [
       ".husky/",
