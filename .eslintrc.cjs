4540f62ff4534714e7cf99e87275e10f6691cb5d
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-04-17 11:51:35 +0300

    fix: remove eslint vue plugin to prevent errors

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript'
+  ]
+}
--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
 import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+// import pluginVue from "eslint-plugin-vue";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/recommended"],
-  {
-    ignores: [
-      ".husky/",
-      "src-tauri/",
-      "node_modules/",
-      "public/",
-      "pnpm-lock.yaml",
-      "*.config.js",
-    ],
-  },
+  {languageOptions: { globals: globals.browser }},
+  ...compat.extends("standard-with-typescript"),
+  // plugin-vue errors "Error while loading rule '@typescript-eslint/dot-notation': You have used a rule which requires parserServices to be generated" (in newly generated config)
+  // ...pluginVue.configs["flat/essential"],
 ];
